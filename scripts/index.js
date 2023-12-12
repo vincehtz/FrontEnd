7e788488347b309f7c8c5b3c5be9781801602f43
Vincent Hertz <vincent.hertz@gmail.com>
2023-12-12 15:22:45 +0100

    vDef

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,21 @@
-// Récupération de l'API
-//  fetch("http://localhost:5678/api/works")
-//  .then((res) => res.json()) // converti le json en JS
-//  .then((data) => genererProjet(data)); // applique la fonction "genereProjet" sur les données récupérées de l'API, après leur conversion de json à JS
+///////////////////////// GENERER LA GALLERIE /////////////////////////
 
 const reponseWorks = await fetch("http://localhost:5678/api/works");
 const gallerie = await reponseWorks.json();
 
-////// GENERER LA GALLERIE ////////
-
-// Fonction qui génère les fiches projet (image+titre).
 function genererProjet(gallerie) {
   const divGallery = document.querySelector(".gallery");
 
-  // Boucle pour parcourir les éléments de la BDD
   for (let i = 0; i < gallerie.length; i++) {
-    // variable qui stocke chaque élément de la BDD individuellement
     const projet = gallerie[i];
 
-    // Création de la balise <figure> pour chaque projet et qui accueillera les images et le titre
     const baliseFigure = document.createElement("figure");
     baliseFigure.classList.add(`projet${projet.id}`);
-    // Création de la balise <img> et des attributs
+
     const baliseImg = document.createElement("img");
     baliseImg.src = projet.imageUrl;
     baliseImg.alt = projet.title;
 
-    // Création de la balise <figcaption>
     const baliseFigcaption = document.createElement("figcaption");
     baliseFigcaption.innerText = projet.title;
 
@@ -35,18 +25,12 @@ function genererProjet(gallerie) {
     baliseFigure.appendChild(baliseFigcaption);
   }
 }
-////// FILTRER LES PROJETS ////////
+///////////////////////// FILTRER LES PROJETS /////////////////////////
 
-// Récupération des balises <button>
 const boutonsAll = document.querySelectorAll(".btn");
-// On déclare une boucle pour parcourir chaque bouton
 for (let i = 0; i < boutonsAll.length; i++) {
-  // On déclare une variable qui stocke chaque bouton individuellement
   const bouton = boutonsAll[i];
-  // On ecoute le clic sur le bouton
   bouton.addEventListener("click", function () {
-    // Bloc de code à éxecuter quand le clic se produit
-    // On déclare une variable qui stocke la méthode "filter"
     const projetsFiltres = gallerie.filter(function (projet) {
       return (
         projet.category.name === bouton.textContent.trim() ||
@@ -62,43 +46,45 @@ for (let i = 0; i < boutonsAll.length; i++) {
   });
 }
 
-////// MODE EDITION (USER LOGGé)////////
+///////////////////////// MODE EDITION (USER LOGGé) /////////////////////////
+
 function modeEdition() {
   const token = localStorage.getItem("token");
+
   if (token) {
     const baliseModeEdition = document.querySelector(".modeEdition");
     const baliseModifier = document.querySelector(".modifier");
     const baliseFiltres = document.querySelector("#portfolio .filtres");
     const baliseLogin = document.querySelector("nav .login");
+
     baliseModeEdition.style.display = "flex";
     baliseModifier.style.display = "flex";
     baliseFiltres.style.display = "none";
     baliseLogin.innerText = "logout";
     baliseLogin.href = "index.html";
+
     baliseLogin.addEventListener("click", function () {
       localStorage.removeItem("token");
     });
   }
 }
 
+///////////////////////// MODALE 1 /////////////////////////
+
 function genererModale1(gallerie) {
+  ////////////// CREER LES BALISES DANS LA MODALE //////////////
   const modaleBody = document.querySelector(".modale1-body");
 
   for (let i = 0; i < gallerie.length; i++) {
-    // variable qui stocke chaque élément de la BDD individuellement
     const projet = gallerie[i];
-    // Récupération de la <div> "modale1-body" dans le code HTML
 
-    // Création de la balise <figure> pour chaque projet et qui accueillera les images
     const baliseFigure = document.createElement("figure");
     baliseFigure.classList.add(`${projet.id}`);
 
-    // Création de la balise <img> et des attributs
     const baliseImg = document.createElement("img");
     baliseImg.src = projet.imageUrl;
     baliseImg.alt = projet.title;
 
-    // Création de la balise <img> et des attributs
     const btnSupprimer = document.createElement("i");
     btnSupprimer.classList.add("fa-solid", "fa-trash-can", "fa-xs");
 
@@ -106,21 +92,18 @@ function genererModale1(gallerie) {
     baliseFigure.appendChild(baliseImg);
     baliseFigure.appendChild(btnSupprimer);
 
+    ////////////// SUPPRIMER PROJET //////////////
     btnSupprimer.addEventListener("click", function () {
       const id = projet.id;
       const token = localStorage.getItem("token");
+
       fetch(`http://localhost:5678/api/works/${id}`, {
         method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
       }).then((res) => {
         if (res.status == 204) {
-          console.log("Projet supprimé");
-          console.log(baliseFigure);
           baliseFigure.remove();
           const projetAccueil = document.querySelector(`.projet${id}`);
-          console.log(projetAccueil);
           if (projetAccueil) {
             projetAccueil.remove();
           }
@@ -130,19 +113,15 @@ function genererModale1(gallerie) {
   }
 }
 
+////////////// FAIRE APPARAITRE/DISPARAITRE LA MODALE //////////////
 const baliseOverlay = document.querySelector(".overlay");
 const baliseModale1 = document.querySelector(".modale1");
 const btnModifier = document.querySelector(".modifier button");
 const modale1Close = document.querySelector(".modale1-close");
-// On écoute le bouton Modifier
+
 btnModifier.addEventListener("click", function () {
-  // Bloc de coe exécuté
-  // Affichage des balises Overlay et Modale1
   baliseOverlay.style.display = "block";
   baliseModale1.style.display = "flex";
-  // On reset le contenu de la balise .modale1-body
-  //modaleBody.innerHTML = "";
-  console.log();
 });
 
 modale1Close.addEventListener("click", function () {
@@ -154,7 +133,7 @@ baliseOverlay.addEventListener("click", function () {
   baliseOverlay.style.display = "none";
   baliseModale1.style.display = "none";
 });
-////// GENERER MODALE 2 ////////
+///////////////////////// MODALE 2 /////////////////////////
 
 function genererModale2() {
   const baliseOverlay = document.querySelector(".overlay");
@@ -171,29 +150,25 @@ function genererModale2() {
     baliseModale1.style.display = "none";
     baliseModale2.style.display = "flex";
 
+    ////////// APERCU DE L'IMAGE //////////
     const imageUploader = document.getElementById("photo");
     const imagePreview = document.querySelector(".modale2-body img");
-
     imageUploader.addEventListener("change", function (event) {
       const file = event.target.files[0];
       if (file) {
-        const reader = new FileReader(); // Création d'un objet FileReader
-
-        // Fonction exécutée lorsque la lecture du fichier est terminée
+        const reader = new FileReader();
         reader.onload = function (e) {
-          imagePreview.src = e.target.result; // Affichage de l'image dans l'aperçu
-          imagePreview.style.display = "block"; // Affichage de l'élément img
+          imagePreview.src = e.target.result;
+          imagePreview.style.display = "block";
           icone.style.display = "none";
           label.style.display = "none";
           text.style.display = "none";
         };
-
-        // Lecture du contenu du fichier en tant que URL de données
         reader.readAsDataURL(file);
       }
     });
   });
-
+  ////////// FERMETURE MODALE 2 //////////
   modale2Close.addEventListener("click", function () {
     baliseOverlay.style.display = "none";
     baliseModale2.style.display = "none";
@@ -222,16 +197,13 @@ function genererModale2() {
   });
 }
 
-// AJOUT PROJET //
+///////////////////////// AJOUT PROJET /////////////////////////
 function ajoutListenerValider() {
-  // Sélection du Formulaire
   const newForm = document.querySelector(".modale2-body form");
-  // Ecoute de l'event submit
   newForm.addEventListener("submit", function (event) {
-    // Empeche le refresh de la page
     event.preventDefault();
-    //récupération du token
 
+    ////////// MESSAGE ALERTE FORMULAIRE //////////
     const image = document.getElementById("photo").files[0];
     const category = parseInt(
       event.target.querySelector("[name=category]").value
@@ -239,18 +211,17 @@ function ajoutListenerValider() {
     const titre = document.getElementById("title").value;
 
     if (!image || !category || !titre) {
-      // Afficher un message à l'utilisateur pour lui indiquer de remplir tous les champs
       alert("Veuillez remplir tous les champs du formulaire.");
-      return; // Arrête l'exécution si un champ est vide
+      return;
     }
 
+    ////////// REQUETE POST //////////
     const token = localStorage.getItem("token");
     const formData = new FormData();
     formData.append("image", image);
     formData.append("title", titre);
     formData.append("category", category);
 
-    // Requete fetch
     fetch("http://localhost:5678/api/works", {
       method: "POST",
       headers: {
@@ -260,8 +231,7 @@ function ajoutListenerValider() {
     })
       .then((res) => {
         if (res.status == 201) {
-          // si status=201 (requete acceptée)...
-          return res.json(); // ...alors convertir la réponse en format JS
+          return res.json();
         }
       })
       .then((data) => {
@@ -271,17 +241,21 @@ function ajoutListenerValider() {
         const icone = document.querySelector(".ajoutPhoto i");
         const label = document.querySelector(".ajoutPhoto label");
         const text = document.querySelector(".ajoutPhoto p");
+
         imagePreview.style.display = "none";
         icone.style.display = "block";
         label.style.display = "block";
         text.style.display = "block";
         baliseModale1.style.display = "flex";
         baliseModale2.style.display = "none";
+
         genererProjet([data]);
         genererModale1([data]);
       });
   });
 }
+
+///////////////////////// APPEL DES FONCTIONS /////////////////////////
 
 genererProjet(gallerie);
 genererModale1(gallerie);
--- scripts/login.js
+++ scripts/login.js
@@ -1,53 +1,35 @@
-//// Déclaration de la fonction pour le Log In ////
-
 function ajoutListenerLogin() {
-  // On récupère le formulaire dans le fichier HTML et on le stocke dans une variable
   const formLogin = document.querySelector(".formulaireLogin");
-  // On écoute l'évenement "submit" avec addEventListener
-  // On passe une fonction anonyme qui s'éxécutera quand l'événement sera déclenché
-  // La variable "event" est fournie par JS et contient les infos liées à l'évènement
+
   formLogin.addEventListener("submit", function (event) {
-    // On empèche le rechargement de la page après le déclenchement de l'evenement
     event.preventDefault();
-    // On déclare une variable qui stocke un objet
-    // L'objet contient les informations (propriétés + valeurs) à envoyer à l'API
+    /////// RECUPERATION INFOS FORMULAIRE ///////
     const login = {
-      // On récupère les éléments rentrés par l'utilisateur au moment du login
       email: event.target.querySelector("[name=email]").value,
       password: event.target.querySelector("[name=password]").value,
     };
-    // On déclare une variable qui converti et stocke l'objet au format JSON
     const chargeUtile = JSON.stringify(login);
-    // On appelle la fonction "fetch" et avec les propriétés pour envoyer les données à l'API
+    /////// REQUETE POST ///////
     fetch("http://localhost:5678/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: chargeUtile,
     })
-      // le premier .then traite la Réponse du serveur
-      // On stocke la réponse de l'API dans une variable "res" pour la convertir en format JS
       .then((res) => {
-        // On déclare une condition selon la réponse de l'API
         if (res.status == 200) {
-          // si status=200 (requete acceptée)...
-          return res.json(); // ...alors convertir la réponse en format JS
+          return res.json();
         } else {
-          // sinon, ne pas convertir la réponse,
-          // et modifier l'élément CSS pour afficher le message d'erreur
+          /////// MESSAGE D'ERREUR ///////
           document.getElementById("error").style.display = "block";
         }
       })
-      // Le deuxieme .then traite un bloc de code à exécuter
       .then((data) => {
-        // On déclare une variable et une fonction anonyme
-        // On déclare une condition
+        ////// RECUPERATION TOKEN ///////
         if (data.token) {
-          // Si un token est présent dans la réponse...
-          // ...alors stoker les infos dans le localStorage...
           localStorage.setItem("token", data.token);
-          // ... et rediriger sur la page d'acceuil
+          ////// REDIRECTION HP LOGGEE ///////
           window.location.href = "index.html";
-        } // Sinon... rien.
+        }
       });
   });
 }
